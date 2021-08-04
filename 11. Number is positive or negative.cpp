38ad70afb8e5fe775e48f0fa86a22ace3c6beeaa
Ayush Singhdeo <73579847+AyushSinghdeo@users.noreply.github.com>
2021-08-05 01:05:48 +0530

    Add files via upload

---
+++ 11. Number is positive or negative.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+cout<<"enter a nuber"<<endl;
+cin>>n;
+if (n>0){
+    cout<<"positive";
+}
+    else if(n<0){
+        cout<<"negative"<<endl;
+    }
+    else{
+        cout<<"zero";
+    }
+
+return 0;
+}
