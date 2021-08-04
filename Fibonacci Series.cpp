7554058bf30849410e2a8888ad7a1a4ebeed1b6a
Ayush Singhdeo <73579847+AyushSinghdeo@users.noreply.github.com>
2021-08-05 00:22:50 +0530

    Update Fibonacci Series.cpp

--- Fibonacci Series.cpp
+++ Fibonacci Series.cpp
@@ -1,12 +1,6 @@
 #include<iostream>
 using namespace std;
 int fib(int n){
-    if (n<2){
-        return 1;
-    }
-    return fib(n+1) + fib(n-1);
-}
-int fib(int n){
     if(n<2){
         return 1;
     }
